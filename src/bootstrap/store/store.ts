ffeab6199c7550d612519d16e12899d4c35d7383
Michal Dziekonski <glibnes@gmail.com>
2019-11-19 00:59:24 +0100

    Gitlab-3 | Add redux-logger

--- src/bootstrap/store/store.ts
+++ src/bootstrap/store/store.ts
@@ -1,4 +1,5 @@
 import { applyMiddleware, compose, createStore, Middleware } from 'redux';
+import { createLogger } from 'redux-logger';
 
 import { createRootReducer } from '@mdziekon/igt-shopping/common/rootState/root.reducer';
 
@@ -6,6 +7,12 @@ interface RequiredInitData {}
 
 const createRootStore = (initData: RequiredInitData) => {
   const middlewares: Middleware[] = [];
+
+  middlewares.push(
+    createLogger({
+      collapsed: true,
+    }),
+  );
 
   const rootReducer = createRootReducer(initData);
 
