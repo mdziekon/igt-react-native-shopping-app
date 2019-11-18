9d29243239542741db29fa0bf00d7a5a217c7e57
Michal Dziekonski <glibnes@gmail.com>
2019-11-18 05:26:37 +0100

    Gitlab-3 | Display the footer & allow to actually add something to cart

--- src/modules/Products/components/ProductDetailsScreen/ProductDetailsScreen.container.tsx
+++ src/modules/Products/components/ProductDetailsScreen/ProductDetailsScreen.container.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
 import {
   ProductDetailsScreenComponent,
@@ -8,27 +9,39 @@ import {
   getCategoryDetails,
   getProductDetails,
 } from '@mdziekon/igt-shopping/common/data/products/mappers.products.data';
+import { cartActionCreators } from '@mdziekon/igt-shopping/common/modules/Cart/actions/cart.actions';
 
 type MappedPropNames = 'category' | 'product';
+type DispatchPropNames = 'onAddToCartPressed';
 
 type MappedProps = Pick<ProductDetailsScreenComponentProps, MappedPropNames>;
+type DispatchProps = Pick<
+  ProductDetailsScreenComponentProps,
+  DispatchPropNames
+>;
 type OwnProps = {};
 
 type ContainerProps = OwnProps &
-  Omit<ProductDetailsScreenComponentProps, keyof MappedProps>;
+  Omit<ProductDetailsScreenComponentProps, keyof MappedProps & DispatchProps>;
 
 export const ProductDetailsScreenContainer: React.FC<ContainerProps> = (
   props,
 ) => {
+  const dispatch = useDispatch();
+
   const productId = props.navigation.getParam('productId');
 
   const product = getProductDetails(productId);
   const category = getCategoryDetails(product.categoryId);
 
+  const onAddToCartPressed = () =>
+    dispatch(cartActionCreators.addItem(productId));
+
   return (
     <ProductDetailsScreenComponent
       category={category}
       product={product}
+      onAddToCartPressed={onAddToCartPressed}
       {...props}
     />
   );
