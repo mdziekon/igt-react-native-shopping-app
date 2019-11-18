59b59b303417372655bfa6cd836cff22e2eb0edf
Michal Dziekonski <glibnes@gmail.com>
2019-11-18 05:25:44 +0100

    Gitlab-3 | Footer should display live data from the cart itself

--- src/common/modules/Footer/components/Footer/Footer.container.tsx
+++ src/common/modules/Footer/components/Footer/Footer.container.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import {
   FooterComponent,
   FooterComponentProps,
 } from '@mdziekon/igt-shopping/common/modules/Footer/components/Footer/Footer.component';
-import { Cart } from '@mdziekon/igt-shopping/common/modules/Cart/models/cart.models';
+import { RootState } from '@mdziekon/igt-shopping/common/rootState/root.reducer';
 
 type MappedPropNames = 'cartItems';
 
@@ -12,13 +13,7 @@ type MappedProps = Pick<FooterComponentProps, MappedPropNames>;
 type OwnProps = Omit<FooterComponentProps, keyof MappedProps>;
 
 export const FooterContainer: React.FC<OwnProps> = (props) => {
-  // TODO: Replace with actual data fetching from the store
-  const cartItems: Cart.Item[] = [
-    {
-      productId: '1',
-      quantity: 1,
-    },
-  ];
+  const cartItems = useSelector((state: RootState) => state.cart.items);
 
   return <FooterComponent cartItems={cartItems} {...props} />;
 };
