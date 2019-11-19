ec51afd455f33324d13aa23810b485c607f7b5e1
Michal Dziekonski <glibnes@gmail.com>
2019-11-19 02:36:15 +0100

    Gitlab-5 | Create CartOrderReceiptScreen

---
+++ src/modules/Cart/components/CartOrderReceiptScreen/CartOrderReceiptScreen.container.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import {
+  CartOrderReceiptScreenComponent,
+  CartOrderReceiptScreenComponentProps,
+} from '@mdziekon/igt-shopping/modules/Cart/components/CartOrderReceiptScreen/CartOrderReceiptScreen.component';
+import { Order } from '@mdziekon/igt-shopping/common/models/order.models';
+
+type MappedPropNames = 'order';
+
+type MappedProps = Pick<CartOrderReceiptScreenComponentProps, MappedPropNames>;
+type OwnProps = {};
+
+type ContainerProps = OwnProps &
+  Omit<CartOrderReceiptScreenComponentProps, keyof MappedProps>;
+
+export const CartOrderReceiptScreenContainer: React.FC<ContainerProps> = (
+  props,
+) => {
+  // const orderId: string = props.navigation.getParam('orderId');
+
+  const order: Order.Order = {
+    orderId: String(Math.random() * 1000000),
+    items: [
+      {
+        productId: '1',
+        quantity: 5,
+        orderPrice: '2.00',
+      },
+      {
+        productId: '3',
+        quantity: 2,
+        orderPrice: '20.00',
+      },
+      {
+        productId: '2',
+        quantity: 7,
+        orderPrice: '5.00',
+      },
+      {
+        productId: '5',
+        quantity: 1,
+        orderPrice: '15.00',
+      },
+    ],
+  };
+
+  return <CartOrderReceiptScreenComponent order={order} {...props} />;
+};
