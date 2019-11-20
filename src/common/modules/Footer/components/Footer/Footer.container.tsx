ff5ca62607dbd97f71953b0509015971e9e54f53
Michal Dziekonski <glibnes@gmail.com>
2019-11-20 01:48:47 +0100

    Gitlab-8 | Use navigation hooks instead of withNavigation HOC (which apparently doesn't like FCs)

--- src/common/modules/Footer/components/Footer/Footer.container.tsx
+++ src/common/modules/Footer/components/Footer/Footer.container.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { useNavigation } from 'react-navigation-hooks';
 
 import {
   FooterComponent,
   FooterComponentProps,
 } from '@mdziekon/igt-shopping/common/modules/Footer/components/Footer/Footer.component';
 import { RootState } from '@mdziekon/igt-shopping/common/rootState/root.reducer';
-import { withNavigation } from 'react-navigation';
 
-type MappedPropNames = 'cartItems';
+type MappedPropNames = 'cartItems' | 'navigation';
 
 type MappedProps = Pick<FooterComponentProps, MappedPropNames>;
 type OwnProps = Omit<FooterComponentProps, keyof MappedProps>;
 
-const FooterContainerBase: React.FC<OwnProps> = (props) => {
+export const FooterContainer: React.FC<OwnProps> = (props) => {
+  const navigation = useNavigation();
   const cartItems = useSelector((state: RootState) => state.cart.items);
 
-  return <FooterComponent cartItems={cartItems} {...props} />;
+  return (
+    <FooterComponent cartItems={cartItems} navigation={navigation} {...props} />
+  );
 };
-
-export const FooterContainer = withNavigation(FooterContainerBase);
